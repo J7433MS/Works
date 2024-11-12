b60c32f78f0e2cfebd8521dbb98bf6ed6602ec40
J7433MS <j7433ms@gmail.com>
2024-11-12 11:24:03 +0900

    web configuration

--- Portfolio(BookApp)/Book-App/src/components/Navbar.jsx
+++ Portfolio(BookApp)/Book-App/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                 <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                     <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                         <p onClick={()=>navigate('/my-profile')} className='hover:text-black cusror-pointer'>My Profile</p>
-                        <p onClick={()=>navigate('/my-appintment')} className='hover:text-black cusror-pointer'>My Appointment</p>
+                        <p onClick={()=>navigate('/my-appointment')} className='hover:text-black cusror-pointer'>My Appointment</p>
                         <p onClick={()=>setToken(false)} className='hover:text-black cusror-pointer'>Logout</p>
                     </div>
                 </div>
