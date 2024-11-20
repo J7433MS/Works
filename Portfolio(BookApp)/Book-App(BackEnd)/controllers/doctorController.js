2baf4f08b72159279df337c8afd09e8a16de3700
J7433MS <j7433ms@gmail.com>
2024-11-20 21:53:26 +0900

    Profile Update, Appointment Update

--- Portfolio(BookApp)/Book-App(BackEnd)/controllers/doctorController.js
+++ Portfolio(BookApp)/Book-App(BackEnd)/controllers/doctorController.js
@@ -15,12 +15,17 @@ const changeAvailability = async (req,res) => {
     }
 }
 
-const doctorList = async () => {
+const doctorList = async (req,res) => {
     try {
+
+        const doctors = await doctorModel.find({}).select(['-password', '-email'])
+
+        res.json({success:true, doctors})
         
     } catch (error) {
-        
+        console.log(error)
+        res.json({success:false,message:error.message})
     }
 }
 
-export {changeAvailability}
+export {changeAvailability, doctorList}
--- Portfolio(BookApp)/Book-App(FrontEnd)/src/context/AppContext.jsx
+++ Portfolio(BookApp)/Book-App(FrontEnd)/src/context/AppContext.jsx
@@ -10,11 +10,10 @@ const AppContexProvider = (props)=>{
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [doctors, setDoctors] = useState([])
+    const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
+    const [userData, setUserData] = useState(false)
 
-    const value = {
-        doctors,
-        currencySymbol
-    }
+    
 
     const getDoctorsData = async () => {
         
@@ -32,9 +31,42 @@ const AppContexProvider = (props)=>{
         }
     }
 
+    const loadUserProfileData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/user/get-profile',{headers:{token}})
+            if (data.success) {
+                setUserData(data.userData)
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    const value = {
+        doctors,
+        currencySymbol,
+        token, setToken,
+        backendUrl,
+        userData, setUserData,
+        loadUserProfileData
+    }
+
     useEffect(()=>{
         getDoctorsData()
     },[doctors])
+
+    useEffect(()=>{
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+    },{token})
 
     return(
         <AppContext.Provider value={value}>
